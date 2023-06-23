7fd355734149b0ed71f89b13563f59b0833893ac
Priya <priya836thakur101@gmail.com>
2023-06-23 10:03:49 +0530

    Committed mt Calcular Project in React

---
+++ src/Components/Button.js
@@ -0,0 +1,8 @@
+import "./Button.css";
+
+const Button = ({className,value, onClick}) =>{
+    return(
+        <button className={className} onClick={onClick}>{value}</button>
+    );
+};
+export default Button;
